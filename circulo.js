405c8a88ce474f7aedc7563425890ee6b271f5f5
unknown <georgina.karen@gmail.com>
2022-10-31 22:33:03 -0500

    primera versión

---
+++ circulo.js
@@ -0,0 +1,8 @@
+for(var i=1;i<=1;i++){
+    document.body.innerHTML+="<marquee behavior='alternate' height='580' SCROLLAMOUNT=8 direction='down'><div id='circulo'></marquee>";   
+}
+
+
+for(var i=1;i<=1;i++){
+    document.body.innerHTML+="<marquee behavior='alternate' SCROLLAMOUNT=9 ><div id='circulo1'></marquee>";   
+}
